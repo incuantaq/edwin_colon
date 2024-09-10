cce8cd0fe1ab283533e79bffafc549917ba5b5fc
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2024-09-10 11:41:30 -0500

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+      colors: {
+        'primary': '#F3AF0C',
+        'secondary': '#4884AA',
+        'gray-15': '#1A1A1A',
+        'gray-55': '#636363',
+      },
+    },
+	},
+	plugins: [],
+}
