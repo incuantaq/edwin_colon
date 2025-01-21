14f7b44bdf118ea48ac0845338d0e84ab66198c7
Sebastian Ramirez <44757100+SebasRDev@users.noreply.github.com>
2025-01-21 14:37:55 -0500

    add vercel json (#4)
    * add vercel json
    
    * change server to hybrid output vercel adapter
    
    * setting output to hybrid
    
    ---------
    
    Co-authored-by: Ana Dallos <ana.dallos@morningstar.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [
     tailwind({
       applyBaseStyles: false,
